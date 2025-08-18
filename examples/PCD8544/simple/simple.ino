e6fb8b266968cc36ca7694ef6154809039015ee0
fkimagem <imagemfk@gmail.com>
2025-08-18 15:52:09 -0300

    update example simple

--- examples/PCD8544/simple/simple.ino
+++ examples/PCD8544/simple/simple.ino
@@ -19,6 +19,13 @@ All text above, and the splash screen must be included in any redistribution
 #include <SPI.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_PCD8544.h>
+#include <displayfk.h>
+
+#define PIN_SCL 11
+#define PIN_DIN 12
+#define PIN_DC 14
+#define PIN_CS 8
+#define PIN_RST 18
 
 // Software SPI (slower updates, more flexible pin options):
 // pin 7 - Serial clock out (SCLK)
@@ -26,249 +33,61 @@ All text above, and the splash screen must be included in any redistribution
 // pin 5 - Data/Command select (D/C)
 // pin 4 - LCD chip select (CS)
 // pin 3 - LCD reset (RST)
-Adafruit_PCD8544 display = Adafruit_PCD8544(7, 6, 5, 4, 3);
+// Adafruit_PCD8544 display = Adafruit_PCD8544(PIN_SCL, PIN_DIN, PIN_DC, PIN_CS, PIN_RST);
+Adafruit_PCD8544 *display = nullptr;
+DisplayFK myDisplay;
 
 #define NUMFLAKES 10
 #define XPOS 0
 #define YPOS 1
 #define DELTAY 2
 
-
 #define LOGO16_GLCD_HEIGHT 16
-#define LOGO16_GLCD_WIDTH  16
+#define LOGO16_GLCD_WIDTH 16
 
-static const unsigned char PROGMEM logo16_glcd_bmp[] =
-{ 0B00000000, 0B11000000,
-  0B00000001, 0B11000000,
-  0B00000001, 0B11000000,
-  0B00000011, 0B11100000,
-  0B11110011, 0B11100000,
-  0B11111110, 0B11111000,
-  0B01111110, 0B11111111,
-  0B00110011, 0B10011111,
-  0B00011111, 0B11111100,
-  0B00001101, 0B01110000,
-  0B00011011, 0B10100000,
-  0B00111111, 0B11100000,
-  0B00111111, 0B11110000,
-  0B01111100, 0B11110000,
-  0B01110000, 0B01110000,
-  0B00000000, 0B00110000 };
-
-void testdrawbitmap(const uint8_t *bitmap, uint8_t w, uint8_t h) {
-  uint8_t icons[NUMFLAKES][3];
-  randomSeed(666);     // whatever seed
- 
-  // initialize
-  for (uint8_t f=0; f< NUMFLAKES; f++) {
-    icons[f][XPOS] = random(display.width());
-    icons[f][YPOS] = 0;
-    icons[f][DELTAY] = random(5) + 1;
-    
-    Serial.print("x: ");
-    Serial.print(icons[f][XPOS], DEC);
-    Serial.print(" y: ");
-    Serial.print(icons[f][YPOS], DEC);
-    Serial.print(" dy: ");
-    Serial.println(icons[f][DELTAY], DEC);
-  }
-
-  while (1) {
-    // draw each icon
-    for (uint8_t f=0; f< NUMFLAKES; f++) {
-      display.drawBitmap(icons[f][XPOS], icons[f][YPOS], bitmap, w, h, BLACK);
-    }
-    display.display();
-    delay(200);
-
-    while(Serial.available()) {
-        switch (Serial.read()) {
-          case 'w':display.setContrast(display.getContrast() + 1);
-                   break;
-          case 's':if(display.getContrast()) display.setContrast(display.getContrast() - 1);
-                     break;
-          case 'e':display.setBias(display.getBias() + 1);
-                   break;
-          case 'd':if(display.getBias()) display.setBias(display.getBias() - 1);
-                   break;
-          case 'R':display.setReinitInterval(10);
-                   break;
-          case 'r':display.initDisplay();
-                   display.setReinitInterval(0);
-                   break;
-        }
-    }
-    Serial.print("contrast (w/s): 0x");
-    Serial.print(display.getContrast(), HEX);
-    Serial.print("   bias (e/d): 0x");
-    Serial.print(display.getBias(), HEX);
-    Serial.print("   reinitialize display (r/R): 0x");
-    Serial.print(display.getReinitInterval(), HEX);
-    Serial.print("   \r");
-
-    // then erase it + move it
-    for (uint8_t f=0; f< NUMFLAKES; f++) {
-      display.drawBitmap(icons[f][XPOS], icons[f][YPOS],  logo16_glcd_bmp, w, h, WHITE);
-      // move it
-      icons[f][YPOS] += icons[f][DELTAY];
-      // if its gone, reinit
-      if (icons[f][YPOS] > display.height()) {
-	icons[f][XPOS] = random(display.width());
-	icons[f][YPOS] = 0;
-	icons[f][DELTAY] = random(5) + 1;
-      }
-    }
-   }
+void loadWidgets()
+{
 }
 
+void screen0()
+{
+  display->clearDisplay();
+  WidgetBase::backgroundColor = CFK_GREY6;
+  // display->fillRect(0, 0, 20, 20, CFK_BLACK);
+  // display->drawRect(10, 10, 20, 20, CFK_BLACK);
+  int posX = 20, posY = 20;
 
-void testdrawchar(void) {
-  display.setTextSize(1);
-  display.setTextColor(BLACK);
-  display.setCursor(0,0);
+  display->drawFastVLine(posX, posY, 80, CFK_BLACK);
+  display->drawFastHLine(posX, posY, 80, CFK_BLACK);
 
-  for (uint8_t i=0; i < 168; i++) {
-    if (i == '\n') continue;
-    display.write(i);
-    //if ((i > 0) && (i % 14 == 0))
-      //display.println();
-  }    
-  display.display();
+  /*display->setCursor(posX,posY);
+  display->setFont(&RobotoRegular10pt7b);
+  display->println("Teste");
+
+  display->setCursor(posX,posY);
+  display->setFont(nullptr);
+  display->println("Teste2");*/
+
+  myDisplay.printText("Teste", posX, posY, TL_DATUM, CFK_BLACK, CFK_WHITE, &RobotoRegular5pt7b);
+  myDisplay.printText("Teste2", posX, posY, BR_DATUM, CFK_BLACK, CFK_WHITE, nullptr);
+
+  display->display();
+  myDisplay.drawWidgetsOnScreen(0);
 }
 
-void testdrawcircle(void) {
-  for (int16_t i=0; i<display.height(); i+=2) {
-    display.drawCircle(display.width()/2, display.height()/2, i, BLACK);
-    display.display();
-  }
-}
-
-void testfillrect(void) {
-  uint8_t color = 1;
-  for (int16_t i=0; i<display.height()/2; i+=3) {
-    // alternate colors
-    display.fillRect(i, i, display.width()-i*2, display.height()-i*2, color%2);
-    display.display();
-    color++;
-  }
-}
-
-void testdrawtriangle(void) {
-  for (int16_t i=0; i<min(display.width(),display.height())/2; i+=5) {
-    display.drawTriangle(display.width()/2, display.height()/2-i,
-                     display.width()/2-i, display.height()/2+i,
-                     display.width()/2+i, display.height()/2+i, BLACK);
-    display.display();
-  }
-}
-
-void testfilltriangle(void) {
-  uint8_t color = BLACK;
-  for (int16_t i=min(display.width(),display.height())/2; i>0; i-=5) {
-    display.fillTriangle(display.width()/2, display.height()/2-i,
-                     display.width()/2-i, display.height()/2+i,
-                     display.width()/2+i, display.height()/2+i, color);
-    if (color == WHITE) color = BLACK;
-    else color = WHITE;
-    display.display();
-  }
-}
-
-void testdrawroundrect(void) {
-  for (int16_t i=0; i<display.height()/2-2; i+=2) {
-    display.drawRoundRect(i, i, display.width()-2*i, display.height()-2*i, display.height()/4, BLACK);
-    display.display();
-  }
-}
-
-void testfillroundrect(void) {
-  uint8_t color = BLACK;
-  for (int16_t i=0; i<display.height()/2-2; i+=2) {
-    display.fillRoundRect(i, i, display.width()-2*i, display.height()-2*i, display.height()/4, color);
-    if (color == WHITE) color = BLACK;
-    else color = WHITE;
-    display.display();
-  }
-}
-   
-void testdrawrect(void) {
-  for (int16_t i=0; i<display.height()/2; i+=2) {
-    display.drawRect(i, i, display.width()-2*i, display.height()-2*i, BLACK);
-    display.display();
-  }
-}
-
-void testdrawline() {  
-  for (int16_t i=0; i<display.width(); i+=4) {
-    display.drawLine(0, 0, i, display.height()-1, BLACK);
-    display.display();
-  }
-  for (int16_t i=0; i<display.height(); i+=4) {
-    display.drawLine(0, 0, display.width()-1, i, BLACK);
-    display.display();
-  }
-  delay(250);
-  
-  display.clearDisplay();
-  for (int16_t i=0; i<display.width(); i+=4) {
-    display.drawLine(0, display.height()-1, i, 0, BLACK);
-    display.display();
-  }
-  for (int8_t i=display.height()-1; i>=0; i-=4) {
-    display.drawLine(0, display.height()-1, display.width()-1, i, BLACK);
-    display.display();
-  }
-  delay(250);
-  
-  display.clearDisplay();
-  for (int16_t i=display.width()-1; i>=0; i-=4) {
-    display.drawLine(display.width()-1, display.height()-1, i, 0, BLACK);
-    display.display();
-  }
-  for (int16_t i=display.height()-1; i>=0; i-=4) {
-    display.drawLine(display.width()-1, display.height()-1, 0, i, BLACK);
-    display.display();
-  }
-  delay(250);
-
-  display.clearDisplay();
-  for (int16_t i=0; i<display.height(); i+=4) {
-    display.drawLine(display.width()-1, 0, 0, i, BLACK);
-    display.display();
-  }
-  for (int16_t i=0; i<display.width(); i+=4) {
-    display.drawLine(display.width()-1, 0, i, display.height()-1, BLACK); 
-    display.display();
-  }
-  delay(250);
-}
-void loadWidgets(){
-
-}
-
-void screen0(){
-    display.clearDisplay();
-    WidgetBase::backgroundColor = CFK_GREY6;
-    display.fillRect(0, 0, 20, 20, CFK_BLACK);
-    display.drawRect(10, 10, 20, 20, CFK_BLACK);
-
-    myDisplay.printText("Media", 30, 30, TL_DATUM, CFK_BLACK, CFK_WHITE, &RobotoRegular10pt7b);
-    display.display();
-    myDisplay.drawWidgetsOnScreen(0);
-}
-
-void setup()   {
+void setup()
+{
   Serial.begin(115200);
-  display.begin();
-  display.setContrast(75);
-  display.clearDisplay();
-  WidgetBase::objTFT = &display; // Reference to object to draw on screen
-  loadWidgets(); // This function is used to setup with widget individualy
-    WidgetBase::loadScreen = screen0; // Use this line to change between screens
-    myDisplay.createTask(); // Initialize the task to read touch and draw
+  display = new Adafruit_PCD8544(PIN_SCL, PIN_DIN, PIN_DC, PIN_CS, PIN_RST);
+  display->begin();
+  display->setContrast(75);
+  display->clearDisplay();
+  WidgetBase::objTFT = display;     // Reference to object to draw on screen
+  loadWidgets();                    // This function is used to setup with widget individualy
+  WidgetBase::loadScreen = screen0; // Use this line to change between screens
+  myDisplay.createTask(true, 5);    // Initialize the task to read touch and draw
 }
 
-
-void loop() {
-  
+void loop()
+{
 }
