f2ff40a9630c6e6c60cc8df099b4642ba7c1d65c
FK Imagem <imagemfk@gmail.com>
2025-08-21 08:01:44 -0300

    update waveshare exemple

--- examples/Embed ESP32S3/Waveshare/Waveshare.ino
+++ examples/Embed ESP32S3/Waveshare/Waveshare.ino
@@ -3,10 +3,10 @@
 #define FORMAT_SPIFFS_IF_FAILED false
 #define DISPLAY_W 800
 #define DISPLAY_H 480
-#define TC_SCL	20
-#define TC_SDA	19
+#define TC_SCL	9
+#define TC_SDA	8
 #define TC_INT	-1
-#define TC_RST	38
+#define TC_RST	-1
 #define TOUCH_ROTATION	ROTATION_NORMAL
 #define TOUCH_MAP_X0	0
 #define TOUCH_MAP_X1	800
@@ -15,6 +15,53 @@
 #define TOUCH_SWAP_XY	false
 #define TOUCH_INVERT_X	false
 #define TOUCH_INVERT_Y	false
+
+#define EXAMPLE_LCD_NAME                        ST7262 // LCD model name
+#define EXAMPLE_LCD_WIDTH                       (800) // LCD width in pixels
+#define EXAMPLE_LCD_HEIGHT                      (480) // LCD height in pixels
+#define EXAMPLE_LCD_COLOR_BITS                  (24)  // Color depth in bits
+#define EXAMPLE_LCD_RGB_DATA_WIDTH              (16)  // Width of RGB data
+#define EXAMPLE_LCD_RGB_COLOR_BITS          (16)    // |      24      |      16       |
+
+#define EXAMPLE_LCD_RGB_TIMING_FREQ_HZ          (16 * 1000 * 1000) // RGB timing frequency
+#define EXAMPLE_LCD_RGB_TIMING_HPW              (4)   // Horizontal pulse width
+#define EXAMPLE_LCD_RGB_TIMING_HBP              (8)   // Horizontal back porch
+#define EXAMPLE_LCD_RGB_TIMING_HFP              (8)   // Horizontal front porch
+#define EXAMPLE_LCD_RGB_TIMING_VPW              (4)   // Vertical pulse width
+#define EXAMPLE_LCD_RGB_TIMING_VBP              (8)   // Vertical back porch
+#define EXAMPLE_LCD_RGB_TIMING_VFP              (8)   // Vertical front porch
+#define EXAMPLE_LCD_RGB_BOUNCE_BUFFER_SIZE  (EXAMPLE_LCD_WIDTH * 10)
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////// Please update the following configuration according to your board spec ////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#define EXAMPLE_LCD_RGB_IO_DISP            (-1)  // RGB display pin number
+#define EXAMPLE_LCD_RGB_IO_VSYNC           (3)   // VSYNC pin number
+#define EXAMPLE_LCD_RGB_IO_HSYNC           (46)  // HSYNC pin number
+#define EXAMPLE_LCD_RGB_IO_DE              (5)   // Data enable pin number
+#define EXAMPLE_LCD_RGB_IO_PCLK            (7)   // Pixel clock pin number
+#define EXAMPLE_LCD_RGB_IO_DATA0           (14)  // RGB data pin 0
+#define EXAMPLE_LCD_RGB_IO_DATA1           (38)  // RGB data pin 1
+#define EXAMPLE_LCD_RGB_IO_DATA2           (18)  // RGB data pin 2
+#define EXAMPLE_LCD_RGB_IO_DATA3           (17)  // RGB data pin 3
+#define EXAMPLE_LCD_RGB_IO_DATA4           (10)  // RGB data pin 4
+#define EXAMPLE_LCD_RGB_IO_DATA5           (39)  // RGB data pin 5
+#define EXAMPLE_LCD_RGB_IO_DATA6           (0)   // RGB data pin 6
+#define EXAMPLE_LCD_RGB_IO_DATA7           (45)  // RGB data pin 7
+#if EXAMPLE_LCD_RGB_DATA_WIDTH > 8
+#define EXAMPLE_LCD_RGB_IO_DATA8           (48)  // RGB data pin 8
+#define EXAMPLE_LCD_RGB_IO_DATA9           (47)  // RGB data pin 9
+#define EXAMPLE_LCD_RGB_IO_DATA10          (21)  // RGB data pin 10
+#define EXAMPLE_LCD_RGB_IO_DATA11          (1)   // RGB data pin 11
+#define EXAMPLE_LCD_RGB_IO_DATA12          (2)   // RGB data pin 12
+#define EXAMPLE_LCD_RGB_IO_DATA13          (42)  // RGB data pin 13
+#define EXAMPLE_LCD_RGB_IO_DATA14          (41)  // RGB data pin 14
+#define EXAMPLE_LCD_RGB_IO_DATA15          (40)  // RGB data pin 15
+#endif
+#define EXAMPLE_LCD_RST_IO                 (-1)  // Reset pin number
+#define EXAMPLE_LCD_BL_IO            (-1)  // Backlight pin number
+#define EXAMPLE_LCD_BL_ON_LEVEL           (1)   // Backlight ON level
+#define EXAMPLE_LCD_BL_OFF_LEVEL !EXAMPLE_LCD_BL_ON_LEVEL // Backlight OFF level
 
 // Prototypes for each screen
 void screen0();
@@ -42,14 +89,14 @@ DisplayFK myDisplay;
 void setup(){
     Serial.begin(115200);
     rgbpanel = new Arduino_ESP32RGBPanel(
-    40, 41, 39, 42,
-    45, 48, 47, 21, 14,
-    5, 6, 7, 15, 16, 4,
-    8, 3, 46, 9, 1,
-    0, 8, 4, 8,
-    0, 8, 4, 8,
-    1, 16000000, false,
-    0, 0, 0);
+    EXAMPLE_LCD_RGB_IO_DE, EXAMPLE_LCD_RGB_IO_VSYNC, EXAMPLE_LCD_RGB_IO_HSYNC, EXAMPLE_LCD_RGB_IO_PCLK,
+    EXAMPLE_LCD_RGB_IO_DATA0, EXAMPLE_LCD_RGB_IO_DATA1, EXAMPLE_LCD_RGB_IO_DATA2, EXAMPLE_LCD_RGB_IO_DATA3, EXAMPLE_LCD_RGB_IO_DATA4,
+    EXAMPLE_LCD_RGB_IO_DATA5, EXAMPLE_LCD_RGB_IO_DATA6, EXAMPLE_LCD_RGB_IO_DATA7, EXAMPLE_LCD_RGB_IO_DATA8, EXAMPLE_LCD_RGB_IO_DATA9, EXAMPLE_LCD_RGB_IO_DATA10,
+    EXAMPLE_LCD_RGB_IO_DATA11, EXAMPLE_LCD_RGB_IO_DATA12, EXAMPLE_LCD_RGB_IO_DATA13, EXAMPLE_LCD_RGB_IO_DATA14, EXAMPLE_LCD_RGB_IO_DATA15,
+    0, EXAMPLE_LCD_RGB_TIMING_HFP, EXAMPLE_LCD_RGB_TIMING_HPW, EXAMPLE_LCD_RGB_TIMING_HBP,
+    0, EXAMPLE_LCD_RGB_TIMING_VFP, EXAMPLE_LCD_RGB_TIMING_VPW, EXAMPLE_LCD_RGB_TIMING_VBP,
+    1, EXAMPLE_LCD_RGB_TIMING_FREQ_HZ, false,
+    0, 0, EXAMPLE_LCD_RGB_BOUNCE_BUFFER_SIZE);
     tft = new Arduino_RGB_Display(DISPLAY_W, DISPLAY_H, rgbpanel, 0 , true);
     tft->begin();
     #if defined(DISP_LED)
